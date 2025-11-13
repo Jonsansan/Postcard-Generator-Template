03b21f2e9d25efaafcb194351cdced2abebce425
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-11-13 13:48:20 -0500

    sejbs8[fhe

---
+++ script.js
@@ -0,0 +1,20 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const postcard = document.getElementById("postcard");
+  const statesEl = document.getElementById("states");
+  const postcardImage = document.getElementById("postcard-image");
+  statesEl.addEventListener("change", function () {
+    const selectedState = statesEl.value;
+    console.log(`State changed to: ${selectedState}`);
+    if (selectedState) {
+      document.getElementById("greeting").textContent = `Hello from ${selectedState}!`;
+      document.getElementById("state-heading").innerHTML = `Welcome to ${selectedState}`;
+      postcard.style.backgroundImage = `url('img/${selectedState.toLowerCase()}.jpg')`;
+      /*       const stateAbbr = selectedState;
+      const imagePath = `img/${stateAbbr.toLowerCase()}.jpg`;
+      postcardImage.setAttribute("src", imagePath);
+      postcardImage.setAttribute("alt", `Postcard from ${stateAbbr}`); */
+    }
+  });
+});
